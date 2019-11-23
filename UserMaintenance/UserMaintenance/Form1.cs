776f50719aff461dd6c4f50781e8d62d9e970e12
Viktor vb <viktor1793@hotmail.com>
2019-11-23 10:45:20 +0100

    hozzaadas commit

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -7,11 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserMaintenance.Entities;
 
 namespace UserMaintenance
 {
     public partial class Form1 : Form
     {
+
+        BindingList<User> users = new BindingList<User>();
+
         public Form1()
         {
             InitializeComponent();
@@ -19,6 +23,20 @@ namespace UserMaintenance
             label1.Text = Resource1.LastName; // label1
             label2.Text = Resource1.FirstName; // label2
             button1.Text = Resource1.Add; // button1
+
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var u = new User()
+            {
+                LastName = textBox1.Text,
+                FirstName = textBox2.Text
+            };
+            users.Add(u);
         }
     }
 }
